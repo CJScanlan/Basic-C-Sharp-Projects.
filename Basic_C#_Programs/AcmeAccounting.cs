fa17d58c65b6da2a66f11ffcdea416e2fb10d309
Carly Scanlan <carlyscanlan@gmail.com>
2021-01-06 12:19:58 -0800

    Add New Project files

---
+++ Basic_C#_Programs/AcmeAccounting.cs
@@ -0,0 +1,10 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Welcome to Acme Accounting Systems \nRemember, we're \"accounting\" on you!");
+        Console.ReadLine();
+    }
+}
---
+++ Basic_C#_Programs/myConsoleProject.cs/myConsoleProject.cs/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace myConsoleProject.cs
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello, " + name + "!");
+            Console.Read();
+        }
+    }
+}
