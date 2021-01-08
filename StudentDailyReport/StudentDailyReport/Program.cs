46e0d591374fdabcd81032877260e3880dcb6a65
Carly Scanlan <carlyscanlan@gmail.com>
2021-01-08 11:37:04 -0800

    Create new files

---
+++ StudentDailyReport/StudentDailyReport/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace StudentDailyReport
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The Tech Academy \nStudent Daily Report");
+
+            Console.WriteLine("What is your name?");
+            string studentName = Console.ReadLine();
+
+            Console.WriteLine(studentName + ", what course are you on?");
+            string studentCourse = Console.ReadLine();
+
+            Console.WriteLine("What page number of " + studentCourse + " are you on?");
+            string pageNum = Console.ReadLine();
+            int pageNumber = Convert.ToInt32(pageNum);
+
+            Console.WriteLine("Do you need help with anything? Please answer \"true\" or \"false.\"");
+            string help = Console.ReadLine();
+            bool studentHelp = Convert.ToBoolean(help);
+
+            Console.WriteLine("Were there any positive experiences you'd like to share? Please give specifics.");
+            string studentExperience = Console.ReadLine();
+
+            Console.WriteLine("Is there any other feedback you'd like to provide? Please be specific.");
+            string studentFeedback = Console.ReadLine();
+
+            Console.WriteLine("How many hours did you study today?");
+            string hours = Console.ReadLine();
+            int studyHours = Convert.ToInt32(hours);
+
+            Console.WriteLine("Thank you for your answers. An instructor will respond to this shortly. Have a great day!");
+            Console.ReadLine();
+
+            
+        }
+    }
+}
---
+++ VariablesAndDataTypes/VariablesAndDataTypes/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VariablesAndDataTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // This is a simple program
+            //Console.WriteLine("What is your name?");
+            //string yourName = Console.ReadLine();
+            //Console.WriteLine("Your name is: " + yourName);
+            //Console.ReadLine();
+
+            //Console.WriteLine("What is your favorite number?");
+            //string favoriteNumber = Console.ReadLine();
+            //int favNum = Convert.ToInt32(favoriteNumber);
+            //int total = favNum + 5;
+            //Console.WriteLine("Your favorite number plus 5 is: " + total);
+            //Console.ReadLine();
+
+            bool isStudying = false;
+            byte hoursWorked = 42;
+            sbyte currentTemperature = -23;
+            char questionMark = '\u2103';
+
+            decimal moneyInBank = 100.5m; //mostly used for financial transactions
+
+            double heightInCentimeters = 211.3025025;
+
+            float secondsLeft = 2.62f; //f at end for float
+
+            short temperatureOnMars = -341;
+
+            string myName = "Carly";
+
+            int currentAge = 25;
+            string yearsOld = currentAge.ToString();
+
+            bool isRaining = true;
+            string rainingStatus = Convert.ToString(isRaining);
+            Console.WriteLine(rainingStatus);
+            Console.ReadLine();
+
+        }
+    }
+}
