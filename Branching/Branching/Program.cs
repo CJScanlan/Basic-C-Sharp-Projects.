e31b5acb2abcf3d18494c1d79300832b7b0812f5
Carly Scanlan <carlyscanlan@gmail.com>
2021-01-11 12:34:50 -0800

    Create new files

---
+++ Branching/Branching/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Branching
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your favorite number?");
+            int favNum = Convert.ToInt32(Console.ReadLine());
+
+            string result = favNum == 12 ? "You have an awesome favorite number." : "You do not have an awesome favorite number.";
+
+            Console.WriteLine(result);
+            Console.ReadLine();
+            //int roomTemp = 70;
+
+            //Console.WriteLine("Hi, what is your name?");
+            //string name = Console.ReadLine();
+
+            //Console.WriteLine("Hi " + name + ", what is the temperature where you are?");
+            //int currentTemp = Convert.ToInt32(Console.ReadLine());
+
+            //if (currentTemp == roomTemp)
+            //{
+            //    Console.WriteLine("It is exactly room temperature.");
+            //}
+            //else if (currentTemp > roomTemp)
+            //{
+            //    Console.WriteLine("It is warmer than room temperature.");
+            //}
+            //else if (roomTemp > currentTemp)
+            //{
+            //    Console.WriteLine("It is colder than room temperature.");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Uhhhhh..... something went wrong.");
+            //}
+            //Console.ReadLine();
+
+            //int currentTemp = 80;
+            //int roomTemp = 70;
+
+            //string comparisonResult = currentTemp == roomTemp ? "It is room temp." : "It is not room temp.";
+
+            //Console.WriteLine(comparisonResult);
+            //Console.ReadLine();
+
+            //if (currentTemp == roomTemp)
+            //{
+            //    Console.WriteLine("It is exactly room temperature.");
+            //}
+            //else if (currentTemp > roomTemp)
+            //{
+            //    Console.WriteLine("It is warmer than room temperature.");
+            //}
+            //else if (roomTemp > currentTemp)
+            //{
+            //    Console.WriteLine("Room temperature is warmer than the current temperature.");
+            //}
+            //else 
+            //{
+            //    Console.WriteLine("It is not exactly room temperature.");
+            //}
+
+        }
+    }
+}
---
+++ ShippingApplication/ShippingApplication/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShippingApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Package Express. Please follow the instructions below.");
+            Console.WriteLine("Enter the package weight:");
+            double pkgWeight = Convert.ToDouble(Console.ReadLine());
+
+            if (pkgWeight > 50.00)
+            {
+                Console.WriteLine("Package is too heavy to be shipped via Package Express. Have a good day.");
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("Enter the package width:");
+                double pkgWidth = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Enter the package height:");
+                double pkgHeight = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Enter the package length:");
+                double pkgLength = Convert.ToDouble(Console.ReadLine());
+
+                if ((pkgWidth + pkgHeight + pkgLength) > 50.00)
+                {
+                    Console.WriteLine("Package is too big to be shipped via Package Express.");
+                    Console.ReadLine();
+                }
+                else
+                {
+                    double pkgCost = ((pkgWidth * pkgLength * pkgHeight) * pkgWeight) / 100.00;
+                    Console.WriteLine("Your estimated total for shipping this package is: $" + pkgCost);
+                    Console.WriteLine("Thank you!");
+                    Console.ReadLine();
+                }
+            }
+        }
+    }
+}
