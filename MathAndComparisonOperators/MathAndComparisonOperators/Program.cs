cb809d04c5150561081a41e2ca7b3f0c4e309a46
Carly Scanlan <carlyscanlan@gmail.com>
2021-01-11 11:08:37 -0800

    Add new files

---
+++ MathAndComparisonOperators/MathAndComparisonOperators/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathAndComparisonOperators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //    int total = 5 + 10;
+            //    int otherTotal = 12 + 22;
+            //    int combined = total + otherTotal;
+            //    Console.WriteLine(combined);
+            //    Console.ReadLine();
+
+            //int difference = 10 - 5;
+            //Console.WriteLine("Ten minus Five" + difference);
+            //Console.ReadLine();
+
+            //int product = 10 * 5;
+            //Console.WriteLine(product);
+            //Console.ReadLine();
+
+            //double quotient = 100.0 / 7.0;
+            //Console.WriteLine(quotient);
+            //Console.ReadLine();
+
+            //int remainder = 10 % 2;
+            //Console.WriteLine(remainder);
+            //Console.ReadLine();
+
+            //bool trueOrFalse = 12 > 5;
+            //Console.WriteLine(trueOrFalse.ToString());
+            //Console.ReadLine();
+
+            int roomTemp = 70;
+            int currentTemp = 70;
+
+            //bool isWarm = currentTemp >= roomTemp;
+            bool isWarm = currentTemp != roomTemp; //equal is ==; not equal is !=
+            Console.WriteLine(isWarm);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ MathOperationsAssignment/MathOperationsAssignment/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathOperationsAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number: ");
+            double number = Convert.ToInt32(Console.ReadLine());
+            double numberMultiply = number * 50.0;
+            Console.WriteLine(number + " multiplied by 50 is " + numberMultiply);
+
+            double numberAdd = number + 25.0;
+            Console.WriteLine(number + " plus 25 is " + numberAdd);
+
+            double numberDivide = number / 12.5;
+            Console.WriteLine(number + " divided by 12.5 is " + numberDivide);
+
+            bool numberGreaterThan = number > 50.0;
+            Console.WriteLine(number + " is greater than 50: " + numberGreaterThan);
+
+            double numberRemainder = number % 7;
+            Console.WriteLine("The remainder of " + number + " divided by 7 is " + numberRemainder);
+            Console.ReadLine();
+        }
+    }
+}
