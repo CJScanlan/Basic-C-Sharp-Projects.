55a8c519c82b30ce57bb9913c23c66f4a6faff50
Carly Scanlan <carlyscanlan@gmail.com>
2021-01-12 10:34:10 -0800

    Add new string program files

---
+++ StringAssignment/StringAssignment/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fName = "Carly ";
+            string mName = "Jean ";
+            string lName = "Scanlan";
+
+            string name = fName + mName + lName;
+            name = name.ToUpper();
+            Console.WriteLine(name);
+
+            StringBuilder sb = new StringBuilder();
+
+            sb.Append("Hi, my name is Carly. ");
+            sb.Append("I am a student at the Tech Academy.");
+            sb.Append(" I enjoy learning C#!");
+
+            Console.WriteLine(sb);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ StringFunctions/StringFunctions/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringFunctions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string name = "Carly";
+            //string quote = "The man said, \"Hello\", ";
+            //string fileName = "C:\\Users\\Carly";
+            //string fileName2 = @"C:\Users\Carly";
+
+            //bool trueOrFalse = name.Contains("s");
+            //trueOrFalse = name.EndsWith("s");
+
+            //int length = name.Length;
+
+            //name = name.ToUpper();
+            //name = name.ToLower();
+
+            //Console.WriteLine(name);
+            //Console.ReadLine();
+
+            StringBuilder sb = new StringBuilder();
+
+            sb.Append("My name is Carly");
+
+            Console.WriteLine(sb);
+            Console.ReadLine();
+        }
+    }
+}
