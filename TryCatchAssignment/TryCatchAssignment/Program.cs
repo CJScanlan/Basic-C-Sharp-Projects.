21ee1cdc916ce24ded2d93078efb47fef20dfc27
Carly Scanlan <carlyscanlan@gmail.com>
2021-01-14 12:56:52 -0800

    Update files

--- TryCatchAssignment/TryCatchAssignment/Program.cs
+++ TryCatchAssignment/TryCatchAssignment/Program.cs
@@ -10,24 +10,24 @@ namespace TryCatchAssignment
     {
         static void Main(string[] args)
         {
-            List<double> numbers = new List<double>();
-            numbers.Add(5.0);
-            numbers.Add(44.0);
-            numbers.Add(72.0);
-            numbers.Add(747.0);
-            numbers.Add(3.0);
-            numbers.Add(11.0);
-            numbers.Add(574.0);
-            numbers.Add(252.0);
+            List<int> numbers = new List<int>();
+            numbers.Add(5);
+            numbers.Add(44);
+            numbers.Add(72);
+            numbers.Add(747);
+            numbers.Add(3);
+            numbers.Add(11);
+            numbers.Add(574);
+            numbers.Add(252);
 
 
             try
             {
                 Console.WriteLine("Pick a number.");
-                double userNum = Convert.ToInt32(Console.ReadLine());
-                foreach (double number in numbers)
+                int userNum = Convert.ToInt32(Console.ReadLine());
+                foreach (int number in numbers)
                 {
-                    double quotient = number / userNum;
+                    int quotient = number / userNum;
                     Console.WriteLine(number + " divided by " + userNum + " equals " + quotient);
                 }
             }
