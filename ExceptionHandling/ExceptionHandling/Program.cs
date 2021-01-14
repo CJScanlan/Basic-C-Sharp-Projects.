a6da0fa1b2903ff556ef37f71e9386623e57a425
Carly Scanlan <carlyscanlan@gmail.com>
2021-01-14 11:26:40 -0800

    Add new files

---
+++ ExceptionHandling/ExceptionHandling/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+            Console.WriteLine("Pick a number.");
+            int number1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Pick a second number.");
+            int number2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Dividing the two...");
+            int number3 = number1 / number2;
+            Console.WriteLine(number1 + " divided by " + number2 + " equals " + number3);
+        }
+        catch (FormatException ex)
+        {
+            Console.WriteLine("Please type a whole number.");
+        }
+        catch (DivideByZeroException ex)
+        {
+            Console.WriteLine("Please don't divide by zero.");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        finally
+        {
+            Console.ReadLine();
+        }
+
+
+    }
+}
+
---
+++ TryCatchAssignment/TryCatchAssignment/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TryCatchAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<double> numbers = new List<double>();
+            numbers.Add(5.0);
+            numbers.Add(44.0);
+            numbers.Add(72.0);
+            numbers.Add(747.0);
+            numbers.Add(3.0);
+            numbers.Add(11.0);
+            numbers.Add(574.0);
+            numbers.Add(252.0);
+
+
+            try
+            {
+                Console.WriteLine("Pick a number.");
+                double userNum = Convert.ToInt32(Console.ReadLine());
+                foreach (double number in numbers)
+                {
+                    double quotient = number / userNum;
+                    Console.WriteLine(number + " divided by " + userNum + " equals " + quotient);
+                }
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine("Please enter a number other than zero.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Please enter a number.");
+            }
+            finally
+            {
+                Console.WriteLine("Wahoo!");
+                Console.ReadLine();
+            }
+
+        }
+    }
+}
