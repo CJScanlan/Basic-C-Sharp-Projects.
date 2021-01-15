b17c9b88c9a4e9e5184febc2cf5b99975f4d7467
Carly Scanlan <carlyscanlan@gmail.com>
2021-01-15 11:34:07 -0800

    Add math class

---
+++ ClassAssignment/ClassAssignment/MathOperation.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAssignment
+{
+    public class MathOperation
+    {
+        public void Addition(int userNum)
+        {
+            int Num3 = userNum + 2;
+            Console.WriteLine(userNum + " plus 2  equals: " + Num3);
+        }
+        
+        public void Subtration(int userNum)
+        {
+            int Num3 = userNum - 2;
+            Console.WriteLine(userNum + " minus 2  equals: " + Num3);
+        }
+
+        public void Multiplication(int userNum)
+        {
+            int Num3 = userNum * 2;
+            Console.WriteLine(userNum + " multiplied by 2  equals: " + Num3);
+        }
+        public int Number { get; set; }
+    }
+}
---
+++ ClassAssignment/ClassAssignment/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number.");
+            int userNum = Convert.ToInt32(Console.ReadLine());
+            
+        }
+    }
+}
