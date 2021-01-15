0505a4c9540ef7d6ce950601a4cefd53a4ba9694
Carly Scanlan <carlyscanlan@gmail.com>
2021-01-15 10:26:44 -0800

    Create card and deck class

---
+++ TwentyOne/TwentyOne/Card.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwentyOne
+{
+    public class Card
+    {
+        public Card()
+        {
+            Suit = "Spades";
+            Face = "Two";
+        }
+        public string Suit { get; set; }
+        public string Face { get; set; }
+    }
+}
