4e63199f1863c4947689c056af7dc50bf4917460
Carly Scanlan <carlyscanlan@gmail.com>
2021-01-16 09:59:08 -0800

    Add method with multi parameters

---
+++ MultiParameterMethod/MultiParameterMethod/Calculation.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiParameterMethod
+{
+    class Calculation
+    {
+        public void Message(int userNum, out int numCalculation, int userNum2 = 0)
+        {
+            numCalculation = userNum / 7;
+
+            if (userNum2 > 0)
+            {
+                int dogYears = userNum2 * 7;
+                Console.WriteLine("Your dog is " + dogYears + " in human years!");
+            }
+        }
+
+    }
+}
---
+++ MultiParameterMethod/MultiParameterMethod/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiParameterMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculation newCalc = new Calculation();
+            Console.WriteLine("Please enter your age.");
+            int userNum = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter your dog's age (optional):");
+
+            int userNum2;
+            if(int.TryParse(Console.ReadLine(), out userNum2))
+            {
+                newCalc.Message(userNum, out int numCalculation, userNum2);
+                Console.WriteLine("You are " + numCalculation + " in dog years!");
+            }
+            else
+            {
+                newCalc.Message(userNum, out int numCalculation);
+                Console.WriteLine("You are " + numCalculation + " in dog years!");
+            }
+            Console.ReadLine();
+        }
+    }
+}
