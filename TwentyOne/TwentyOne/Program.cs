b7fdc9d348528d31372df00c1a621cc1a7a69ee9
Carly Scanlan <carlyscanlan@gmail.com>
2021-01-17 12:06:12 -0800

    Create class and superclass

--- TwentyOne/TwentyOne/Program.cs
+++ TwentyOne/TwentyOne/Program.cs
@@ -10,6 +10,7 @@ namespace TwentyOne
     {
         static void Main(string[] args)
         {
+
             Deck deck = new Deck();
             deck.Shuffle(3);
 
---
+++ TwentyOne/TwentyOne/TwentyOneGame.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwentyOne
+{
+    public class TwentyOneGame : Game //inheritance
+    {
+        public void Play()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
