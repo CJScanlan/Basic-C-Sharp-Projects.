1f3b8b1a8655e227e912d98f92a6584f5f82bdf3
Carly Scanlan <carlyscanlan@gmail.com>
2021-01-17 11:17:45 -0800

    Update method call

--- VoidMethod/VoidMethod/Program.cs
+++ VoidMethod/VoidMethod/Program.cs
@@ -13,6 +13,7 @@ namespace VoidMethod
             Operation exObject = new Operation();
             Console.WriteLine("Enter a number.");
             int userNum = Convert.ToInt32(Console.ReadLine());
+            exObject.Addition(12, 16);
             exObject.Addition(userNum, num2: 12);
             Console.ReadLine();
         }
