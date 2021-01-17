120bc445ca5da0ca0d5be94378a59f00880dc8dc
Carly Scanlan <carlyscanlan@gmail.com>
2021-01-17 11:15:46 -0800

    Add new method to program

---
+++ VoidMethod/VoidMethod/Operation.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoidMethod
+{
+    class Operation
+    {
+        public void Addition(int userNum, int num2)
+        {
+            int result = userNum + 7;
+            Console.WriteLine(num2);
+        }
+    }
+}
---
+++ VoidMethod/VoidMethod/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoidMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Operation exObject = new Operation();
+            Console.WriteLine("Enter a number.");
+            int userNum = Convert.ToInt32(Console.ReadLine());
+            exObject.Addition(userNum, num2: 12);
+            Console.ReadLine();
+        }
+    }
+}
