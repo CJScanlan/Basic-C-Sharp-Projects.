ca26c991bed36c58ada71961a84fcd67bc7dd37d
Carly Scanlan <carlyscanlan@gmail.com>
2021-01-18 11:17:11 -0800

    Create generic type parameter and loop

--- AbstractClassAssignment/AbstractClassAssignment/Employee.cs
+++ AbstractClassAssignment/AbstractClassAssignment/Employee.cs
@@ -1,9 +1,14 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace AbstractClassAssignment
 {
-    public class Employee : Person, IQuittable
+    public class Employee<T> : Person, IQuittable
     {
+        public List<T> Things { get; set; }
         public int Id { get; set; }
         public override void SayName()
         {
@@ -14,13 +19,13 @@ namespace AbstractClassAssignment
             Console.WriteLine("I quit!");
         }
 
-        public static bool operator ==(Employee a, Employee b)
+        public static bool operator ==(Employee<T> a, Employee<T> b)
         {
             bool equal = a.Id == b.Id;
             return equal;
 
         }
-        public static bool operator !=(Employee a, Employee b)
+        public static bool operator !=(Employee<T> a, Employee<T> b)
         {
             bool equal = a.Id != b.Id;
             return equal;
--- AbstractClassAssignment/AbstractClassAssignment/Program.cs
+++ AbstractClassAssignment/AbstractClassAssignment/Program.cs
@@ -10,18 +10,35 @@ namespace AbstractClassAssignment
     {
         static void Main(string[] args)
         {
-            Employee employee1 = new Employee() { firstName = "Sample", lastName = "Student", Id = 1 };
-            Employee employee2 = new Employee() { firstName = "Bob", lastName = "Saget", Id = 2 };
-            Employee employee3 = new Employee() { firstName = "Bobby", lastName = "Saget", Id = 2 };
-            employee1.SayName();
-            IQuittable employee4 = new Employee();
-            Console.WriteLine("Employee 2 says: ");
-            employee2.Quit();
+            //Employee employee1 = new Employee() { firstName = "Sample", lastName = "Student", Id = 1 };
+            //Employee employee2 = new Employee() { firstName = "Bob", lastName = "Saget", Id = 2 };
+            //Employee employee3 = new Employee() { firstName = "Bobby", lastName = "Saget", Id = 2 };
+            //employee1.SayName();
+            //IQuittable employee4 = new Employee();
+            //Console.WriteLine("Employee 2 says: ");
+            //employee2.Quit();
 
-            bool equal = employee1 == employee2;
-            Console.WriteLine(equal);
-            equal = employee2 == employee3;
-            Console.WriteLine(equal);
+            //bool equal = employee1 == employee2;
+            //Console.WriteLine(equal);
+            //equal = employee2 == employee3;
+            //Console.WriteLine(equal);
+            //Console.ReadLine();
+
+            Employee<string> strThings = new Employee<string>();
+            strThings.Things = new List<string>() { "Pencil", "Desk", "Computer", "Crackers" };
+
+            Employee<int> intThings = new Employee<int>();
+            intThings.Things = new List<int>() { 100, 101, 102, 103 };
+
+            foreach (string thing in strThings.Things)
+            {
+                Console.WriteLine(thing);
+            }
+
+            foreach (int thing in intThings.Things)
+            {
+                Console.WriteLine(thing);
+            }
             Console.ReadLine();
         }
     }
