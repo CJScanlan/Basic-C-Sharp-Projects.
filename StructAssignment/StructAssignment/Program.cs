ca77e97d0fc5f259d57a9cc99debf6da96ec8f69
Carly Scanlan <carlyscanlan@gmail.com>
2021-01-19 19:25:51 -0800

    Create struct

---
+++ StructAssignment/StructAssignment/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number number = new Number();
+            number.Amount = 10;
+            Console.WriteLine(number.Amount);
+            Console.ReadLine();
+        }
+    }
+}
