e206647652168b970765a368da4e8aebfc3e6300
Carly Scanlan <carlyscanlan@gmail.com>
2021-01-19 16:28:15 -0800

    Create enums

--- TwentyOne/TwentyOne/Program.cs
+++ TwentyOne/TwentyOne/Program.cs
@@ -10,13 +10,6 @@ namespace TwentyOne
     {
         static void Main(string[] args)
         {
-            Game game = new TwentyOneGame();
-            game.Players = new List<Player>();
-            Player player = new Player();
-            player.Name = "Carly";
-            //game += player;
-            //game -= player;
-
             Deck deck = new Deck();
             deck.Shuffle(3);
 
