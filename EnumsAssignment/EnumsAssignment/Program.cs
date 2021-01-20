216c3f0dc408e491e773588bdd0e17d7e088c98b
Carly Scanlan <carlyscanlan@gmail.com>
2021-01-19 16:50:58 -0800

    Create days of week enum

---
+++ EnumsAssignment/EnumsAssignment/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumsAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Enter the current day of the week.");
+                string strUserInput = Console.ReadLine();
+                DaysOfTheWeek userInput = (DaysOfTheWeek) Enum.Parse(typeof(DaysOfTheWeek), strUserInput);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Please enter an actual day of the week.");
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+        }
+        public enum DaysOfTheWeek
+        {
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday,
+            Sunday
+        }
+    }
+}
